ff32eb0fcbbcff941488f10c909b45cbd6b7d69c
Anmol <anmolawasthi117@gmail.com>
2023-10-06 15:39:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var tl = gsap.timeline()
+tl.from("#nav",{
+    opacity:0,
+    delay:0.3,
+})
+tl.from("#nav h1,#nav h2,#nav h3,#nav h4",{
+    y:-100,
+    opacity: 0,
+    duration: 0.8,
+    delay:0.5,
+    stagger:0.3,
+})
+tl.from("#left h1",{
+    x:-500,
+    opacity:0,
+    duration:0.5,
+    stagger:0.5,
+})
+tl.from("#right img",{
+    scale:2,
+    opacity:0,
+    duration:0.5,
+})
+gsap.from("#page2 .box",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:"#page2 .box",
+        scroller:"body",
+
+    }
+})
